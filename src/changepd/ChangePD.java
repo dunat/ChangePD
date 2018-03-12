f874ee7025081d7ba603c9a753dcdeaa57415733
dunat <dunat.aqui@gmail.com>
2018-03-12 15:04:34 +0100

    first_release

--- src/changepd/ChangePD.java
+++ src/changepd/ChangePD.java
@@ -5,6 +5,8 @@
  */
 package changepd;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +19,18 @@ public class ChangePD {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
                     CalcoloShift c = new CalcoloShift();
                     //carico il file
-                    List<Time_series> l = c.load("/home/rodman/Scrivania/test_sample.csv");
+                    List<Time_series> li = c.load("/home/rodman/Scrivania/Sperimentazione_twitter/twita_tri_cum.csv");
                     //normalizzo le serie
-                    c.normalize(l);
+                    c.normalize(li);
+                    ArrayList<Time_series> l = c.filtra_hashtag("/home/rodman/Scrivania/Sperimentazione_twitter/11012018_esecuzione_twitter/hash_piu_frequenti.csv", li);
+                    li.clear();
                     
+                    System.out.println("Permutazioni delle serie...");
                     List<List<Time_series>> liste_campione = new ArrayList<>();
                     for(int i = 0; i < l.size(); i++){
                         //calcolo la shift mean per ogni serie
@@ -35,6 +41,7 @@ public class ChangePD {
                         liste_campione.add(liste);
                     }
                     
+                    System.out.println("Calcolo mean shift dei campioni...");
                     for(int i = 0; i < liste_campione.size(); i++){
                         for(int j = 0; j < liste_campione.get(0).size(); j++){
                             //creo le mean schift per serie di ogni campione
@@ -45,10 +52,28 @@ public class ChangePD {
                     //System.out.println("Campioni: ");
                     //System.out.println(liste_campione.get(0).toString());
                     //calcolo i pvalue
+                    System.out.println("Calcolo pvalue...");
                     for(int i = 0; i < l.size(); i++){
                         c.computePValue(l.get(i), liste_campione.get(i));
                     }
-                    System.out.println(l.toString());
+                    
+                    BufferedWriter w = new BufferedWriter(new FileWriter("/home/rodman/Scrivania/Sperimentazione_twitter/032018_twitter/pvalue_cum.csv"));
+                    for(int i = 0; i < l.size(); i++){
+                        
+                        w.write(String.valueOf(l.get(i).getInd()));
+                        w.write(",");
+                        w.write(l.get(i).getWord());
+                        
+                        for(int j = 0; j < l.get(i).get_pvalue().size(); j++){
+                            w.write(",");
+                            w.write(String.valueOf(l.get(i).get_pvalue().get(j)));
+                            
+                        }
+                        w.write("\n");
+                    }
+                    w.close();
+                    //System.out.println(l.toString());
+
     }
     
 }
